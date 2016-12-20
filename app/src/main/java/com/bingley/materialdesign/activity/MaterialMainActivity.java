7789b61e5ee9e90c5e14b80a59a4fb52ada65291
linmb@qq.com <linmb@qq.com>
2016-12-20 10:25:53 +0800

    先从零学起

--- app/src/main/java/com/bingley/materialdesign/activity/MaterialMainActivity.java
+++ app/src/main/java/com/bingley/materialdesign/activity/MaterialMainActivity.java
@@ -1,16 +1,14 @@
 package com.bingley.materialdesign.activity;
 
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.content.Intent;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.bingley.materialdesign.R;
 import com.bingley.materialdesign.base.BaseActivity;
-import com.bingley.materialdesign.view.DividerItemDecoration;
 
 import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * Created by Mr.bingley
@@ -20,39 +18,32 @@ import butterknife.Bind;
 public class MaterialMainActivity extends BaseActivity {
     @Bind(R.id.toolbar)
     Toolbar mToolbar;
-    @Bind(R.id.recyclerView)
-    RecyclerView mRecyclerView;
 
     @Override
     protected int getContentView() {
-        return R.layout.act_listview;
+        return R.layout.act_material;
     }
 
     @Override
     protected void initWidget() {
         super.initWidget();
-        /*setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
-
-        mRecyclerView.setHasFixedSize(true);
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-        //mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getActivity(), onItemClickListener));
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        String[] myDataset = new String[]{"RecycleView",
-                "TextInputLayout", "CardView", "AppBar & TabLayout","Bottom Tab"
-        };
-        /*MyAdapter mAdapter = new MyAdapter(getActivity(), myDataset);
-        mRecyclerView.setAdapter(mAdapter);*/
     }
 
 
+    @OnClick({ R.id.ll_hide_toolbar})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ll_hide_toolbar:
+                startActivity(new Intent(this, HideToolbarActivty.class));
+                break;
+        }
+    }
 }
