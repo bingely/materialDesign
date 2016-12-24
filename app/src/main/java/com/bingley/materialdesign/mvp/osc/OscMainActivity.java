5af011355ea0926067a07e6eeb9e4655dd6d0ff1
linmb@qq.com <linmb@qq.com>
2016-12-25 00:14:04 +0800

    DialogDemo

---
+++ app/src/main/java/com/bingley/materialdesign/mvp/osc/OscMainActivity.java
@@ -0,0 +1,20 @@
+package com.bingley.materialdesign.mvp.osc;
+
+import com.bingley.materialdesign.R;
+import com.bingley.materialdesign.base.BaseActivity;
+
+/**
+ * 我想实现点击下面某个模块会刷新界面功能
+ * 当点击某个按钮的时候，直接回到首页
+ * Author:  Mr.bingley
+ * Version:
+ * Date:  2016/12/22
+ */
+
+public class OscMainActivity extends BaseActivity {
+    @Override
+    protected int getContentView() {
+        return R.layout.act_osc_main;
+    }
+
+}
