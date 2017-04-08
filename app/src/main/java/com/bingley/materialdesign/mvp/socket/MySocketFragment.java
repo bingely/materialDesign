b6ab554064b09e6d1bebee1a5a4604295ea2df2d
linmb@qq.com <linmb@qq.com>
2017-04-08 14:51:11 +0800

    upload

--- app/src/main/java/com/bingley/materialdesign/mvp/socket/MySocketFragment.java
+++ app/src/main/java/com/bingley/materialdesign/mvp/socket/MySocketFragment.java
@@ -9,29 +9,13 @@ import android.view.ViewGroup;
 import com.bingley.materialdesign.AppConfig;
 import com.bingley.materialdesign.R;
 import com.bingley.materialdesign.base.BaseFragment;
-import com.bingley.materialdesign.mvp.socket.tcp.SocketConnection;
 import com.bingley.materialdesign.utils.LogUtils;
-import com.koushikdutta.async.http.AsyncHttpClient;
-import com.koushikdutta.async.http.socketio.Acknowledge;
-import com.koushikdutta.async.http.socketio.ConnectCallback;
-import com.koushikdutta.async.http.socketio.EventCallback;
-import com.koushikdutta.async.http.socketio.JSONCallback;
-import com.koushikdutta.async.http.socketio.SocketIOClient;
 import com.vilyever.socketclient.SocketClient;
-import com.vilyever.socketclient.helper.SocketClientDelegate;
-import com.vilyever.socketclient.helper.SocketClientReceivingDelegate;
-import com.vilyever.socketclient.helper.SocketClientSendingDelegate;
-import com.vilyever.socketclient.helper.SocketPacket;
-import com.vilyever.socketclient.helper.SocketPacketHelper;
-import com.vilyever.socketclient.helper.SocketResponsePacket;
-import com.vilyever.socketclient.util.CharsetUtil;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
 import butterknife.OnClick;
 import io.socket.client.IO;
@@ -46,7 +30,7 @@ import io.socket.emitter.Emitter;
 
 public class MySocketFragment extends BaseFragment {
 
-    private SocketManager socketManger;
+    private SocketManager2 socketManger;
     private SocketClient localSocketClient;
     private Socket socket;
 
@@ -58,87 +42,24 @@ public class MySocketFragment extends BaseFragment {
     @Override
     protected void initWidget(View root) {
         super.initWidget(root);
-
-
-        socketManger = new SocketManager();
-
-       try {
-            socket = IO.socket("http://192.168.10.234:60000");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
-
-            @Override
-            public void call(Object... args) {
-                socket.emit("foo", "hi");
-                //socket.disconnect();
-
-                LogUtils.e("call0");
-
-                /*JSONObject obj = (JSONObject)args[0];
-                LogUtils.e("call0"+obj.toString());*/
-            }
-
-        }).on("event", new Emitter.Listener() {
-
-            @Override
-            public void call(Object... args) {
-                LogUtils.e("event");
-            }
-
-        }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
-
-            @Override
-            public void call(Object... args) {
-                LogUtils.e("diconnect");
-            }
-
-        });
+        socketManger = new SocketManager2();
+        localSocketClient = socketManger.getLocalSocketClient();
     }
 
     @OnClick({R.id.bt_connet, R.id.bt_send})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_connet:
-               //socketManger.connect(localSocketClient);
-
-                socket.connect();
-
-                socket.on("login", onLogin);
+               socketManger.connect(localSocketClient);
                 LogUtils.e("loging");
 
                 break;
             case R.id.bt_send:
-                //localSocketClient.sendString("hello");
-                JSONObject obj1 = new JSONObject();
-                try {
-                    obj1.put("hello", "server");
-                    obj1.put("binary", "hljlasdf");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                socket.emit("foo", obj1);
+
+                localSocketClient.sendString("V0.1000000640000100100000000000000000{\"1\":\"1111\",\"91\":\"a123456\"}");
                 break;
         }
     }
-
-
-    private Emitter.Listener onLogin = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            LogUtils.e("call");
-           /* JSONObject data = (JSONObject) args[0];
-
-            int numUsers;
-            try {
-                numUsers = data.getInt("numUsers");
-            } catch (JSONException e) {
-                return;
-            }*/
-
-        }
-    };
 
 
 }
