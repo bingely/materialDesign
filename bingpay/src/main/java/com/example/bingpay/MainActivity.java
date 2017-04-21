f58ebea7029a09c774c81f7c560a4f6e23abaac7
bingley <linmb@qq.com>
2017-04-21 18:00:56 +0800

    dafsd

--- bingpay/src/main/java/com/example/bingpay/MainActivity.java
+++ bingpay/src/main/java/com/example/bingpay/MainActivity.java
@@ -14,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       /*
+        这个项目难在什么地方呢？？？
+        第一 socket通讯（我存在无法获取消息的苦恼）
+        第二 K线图的绘制
+        第三  协议的理解
+
+        这尼玛不是人做的事啊，太尼玛的难了。
+        时间在慢慢的流逝当中，而自己的进度又是没有进展啊，苦恼苦恼
+
+        */
     }
 }
